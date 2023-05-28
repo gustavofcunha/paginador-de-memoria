a504bd3d0963eb2dff39297fae9d6c34ae45dbed
Gabrimartins <gabrimartinsjuarez@gmail.com>
2023-05-28 18:05:44 -0300

    Método pager_create

--- mempager/src/pager.c
+++ mempager/src/pager.c
@@ -1,3 +1,5 @@
+#include <sys/mman.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include "pager.h"
@@ -8,20 +10,20 @@
 #include "log.h"
 
 //estrutura de dados que representa um quadro na memoria fisica
-typedef struct Quadro{
+typedef struct Quadro {
     pid_t pid; //id do processo que utiliza o quadro
     int numPagina;
     bool disponivel; //true se disponivel, false caso contrario
-}Quadro;
+} Quadro;
 
 //estrutura de dados que representa uma tabela de paginas de um processo
-typedef struct TabelaDePaginas{
+typedef struct TabelaDePaginas {
 	int qtdPaginas; // Usado como tamanho dos dois vetores quadros e blocos.
-	int *quadros; 
-	int *blocos; 
+	int *quadros;
+	int *blocos;
 } TabelaDePaginas;
 
-typedef struct ListaDeTabelas{
+typedef struct ListaDeTabelas {
 	pid_t pid;
 	TabelaDePaginas *tabela;
 } ListaDeTabelas;
@@ -41,38 +43,56 @@ int tamanhoListaDeTabelas;
 
 //gu-
 //inicializacao do paginador
-void pager_init(int nframes, int nblocks){
+void pager_init(int nquadros, int nblocos){
     int i;
 
     //inicializacao do vetor de quadros da memoria fisica
-    numeroQuadros = nframes;
-    quadros = (Quadro*) malloc (nframes * sizeof(Quadro));
-    for(i=0; i<nframes; i++){
+    numeroQuadros = nquadros;
+    quadros = (Quadro*) malloc(nquadros* sizeof(Quadro));
+    for(i=0; i<nquadros; i++){
         quadros[i].disponivel = true;
         quadros[i].numPagina = 0;
         quadros[i].pid = -1;
     }
 
     //inicializacao do vetor de blocos
-    tamanhoVetorBlocos = nblocks;
-    qtdBlocosLivres = nblocks;
-    vetorDeBlocos = (int*) malloc (nblocks * sizeof(int));
-    for(i = 0; i < nblocks; i++){
+    tamanhoVetorBlocos = nblocos;
+    qtdBlocosLivres = nblocos;
+    vetorDeBlocos = (int*) malloc(nblocos* sizeof(int));
+    for(i = 0; i < nblocos; i++){
 		vetorDeBlocos[i] = 0; //setando todas os blocos como vazios
     }
 
     //inicializacao da lista de tabela de paginas
     tamanhoListaDeTabelas = 1;
-    listaDeTabelas = (ListaDeTabelas*) malloc (tamanhoListaDeTabelas * sizeof(ListaDeTabelas));
+    listaDeTabelas = (ListaDeTabelas*) malloc(tamanhoListaDeTabelas* sizeof(ListaDeTabelas));
 }
 //-gu
 
-void pager_create(pid_t pid){
-    int i, j, numeroDePaginas, aux = 0;
-	// Calcula o número de páginas dos vetores frames e blocks.
-	numeroDePaginas = (UVM_MAXADDR - UVM_BASEADDR + 1) / sysconf(_SC_PAGESIZE);
 
+void pager_create(pid_t pid)
+{
+    int i, j, qtdPaginas;
+    // Calcula o número de páginas dos vetores quadros e blocos.
+    qtdPaginas = (UVM_MAXADDR - UVM_BASEADDR + 1) / sysconf(_SC_PAGESIZE);
 
+    // Procurando tabela vazia na lista de tabelas.
+    for (i = 0; i < tamanhoListaDeTabelas; i++){
+        // Se a tabela estiver vazia.
+        if (listaDeTabelas[i].tabela == NULL){
+            listaDeTabelas[i].pid = pid;
+            listaDeTabelas[i].tabela = malloc(sizeof(TabelaDePaginas));
+            listaDeTabelas[i].tabela->qtdPaginas = qtdPaginas;
+            listaDeTabelas[i].tabela->quadros = malloc(qtdPaginas* sizeof(int));
+            listaDeTabelas[i].tabela->blocos = malloc(qtdPaginas* sizeof(int));
 
+            // Seta os valores para -1 (convenção de vazio).
+            for (j = 0; j < qtdPaginas; j++){
+                listaDeTabelas[i].tabela->quadros[j] = -1;
+                listaDeTabelas[i].tabela->blocos[j] = -1;
+            }
 
+            break;
+        }
+    }
 }
