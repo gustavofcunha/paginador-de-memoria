2b3afa8c62a59df79bc7d8946c09c040df1207ed
gustavofcunha <gustavocunha182020@gmail.com>
2023-05-28 16:00:02 -0300

    implementacao do metodo pager_init

--- mempager/src/pager.c
+++ mempager/src/pager.c
@@ -13,20 +13,52 @@ typedef struct Quadro{
     bool disponivel; //true se disponivel, false caso contrario
 }Quadro;
 
+typedef struct TabelaDePaginas{
+	int qtdPaginas; // Usado como tamanho dos dois vetores quadros e blocos.
+	int *quadros; 
+	int *blocos; 
+} TabelaDePaginas;
+
+typedef struct ListaDeTabelas{
+	pid_t pid;
+	TabelaDePaginas *tabela;
+} ListaDeTabelas;
+
 //vetor de quadros da memoria fisica
 Quadro *quadros;
 int numeroQuadros;
 
+//vetor de blocos
+int *vetorDeBlocos;
+int qtdBlocosLivres; // informa quantos blocos ainda estão livres.
+int tamanhoVetorBlocos;
+
+//vetor de tabelas
+ListaDeTabelas *listaDeTabelas;
+int tamanhoListaDeTabelas;
 
 //inicializacao do paginador
 void pager_init(int nframes, int nblocks){
+    int i;
 
     //inicializacao do vetor de quadros da memoria fisica
     numeroQuadros = nframes;
     quadros = (Quadro*) malloc (nframes * sizeof(Quadro));
-    for(int i=0; i<nframes; i++){
+    for(i=0; i<nframes; i++){
         quadros[i].disponivel = true;
         quadros[i].numPagina = 0;
         quadros[i].pid = -1;
     }
+
+    //inicializacao do vetor de blocos
+    tamanhoVetorBlocos = nblocks;
+    qtdBlocosLivres = nblocks;
+    vetorDeBlocos = (int*) malloc (nblocks * sizeof(int));
+    for(i = 0; i < nblocks; i++){
+		vetorDeBlocos[i] = 0; //setando todas os blocos como vazios
+    }
+
+    //inicializacao da lista de tabela de paginas
+    tamanhoListaDeTabelas = 1;
+    listaDeTabelas = (ListaDeTabelas*) malloc (tamanhoListaDeTabelas * sizeof(ListaDeTabelas));
 }
